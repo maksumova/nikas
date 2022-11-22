29e3ddcfed373e776bb6bd19559aed789e646a4a
Tima <nabdulakhatova@gmail.com>
2022-11-22 20:10:50 +0600

    //print

--- huhuhuh/main.swift
+++ huhuhuh/main.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+
+// if alse if
+
+// switch
+
+//
+
+func printSmth(string: String ){
+    if string == ""{
+    }
+    
+    let readline = readLine()!
+    
+    printSmth (string: readline)
+    
+    func printSmth2 (string: String){
+        switch string {
+        case "": print ("empty")
+        case "me": print ("you")
+        default: print ("")
+        }
+        
+    }
+    
+    printSmth2(string: readline)
+    
+}
+    
+let a = 44
+switch a {
+case 0...10: print("small")
+case 11...20: print("medium")
+case 21...30: print("big")
+default: print("any")
+}
+
